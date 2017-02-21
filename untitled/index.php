2b855058b30b4a9b7097f96b97f70499e2fe06a1
Unknown <greenwaets@gmail.com>
2017-02-21 11:52:39 +0100

    Changed index

--- untitled/index.php
+++ untitled/index.php
@@ -1,2 +1,17 @@
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAaG47nnbYjQtbdkaDCI-qMZ_QkLUn2rWjm9NIfzP65HNT14Po8UgbLKY" alt="">
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
+</head>
+<body>
 <button type="button">Jouer !</button>
+</body>
