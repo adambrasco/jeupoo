0efad10e2ada032fe743f915b6ee84946b7fabf7
Unknown <greenwaets@gmail.com>
2017-02-23 22:46:05 +0100

    rework index

--- untitled/index.php
+++ untitled/index.php
@@ -1,39 +1,21 @@
 <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <!-- Css -->
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
 
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-            crossorigin="anonymous"></script>
+
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="panel1 col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <img src="img/warcraft-logo.png" class="center-block" style="margin-bottom: -150px; margin-top: -50px;" alt="">
-                    <p class="block-center"><h2>Prêt à vous lancer dans l'aventure ?</h2>
-                    <ul><h3>Les règles sont simples :</h3>
-                        <li>Choississez votre classe</li>
-                        <li>Découvrez le donjon</li>
-                        <li>Faites évoluer votre personnage</li>
-                    </ul>
-                    </p>
-                    <a href="view/choix.php">
-                        <button type=" button" class="btn-warning center-block">Jouer !</button>
-                    </a>
-                </div>
-            </div>
+<div class="startStage">
+    <div>
+        <h1><i>H</i><i>E</i><i>R</i><i>O</i><i>E</i><i>S</i></h1>
+        <p class="warning">Prêt à partir à l'aventure ?</p>
+        <h2>Règles du jeu :</h2>
+        <div>
+            <p>Choisissez votre héro</p>
+            <p>Explorez les étages du donjon</p>
+            <p>Battez les monstres et obtenez des récompenses !</p>
         </div>
+        <label for="start" onClick="" id="one"><a href="view/choix.php">LANCER LE JEU</a></label>
     </div>
 </div>
 </body>
