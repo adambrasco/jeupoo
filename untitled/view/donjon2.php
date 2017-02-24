5a1132b5a32af7989b914e8175bde1c21e3fe815
Unknown <louisalexandrebricq@yahoo.fr>
2017-02-24 16:14:24 +0100

    redirection header

--- untitled/view/donjon2.php
+++ untitled/view/donjon2.php
@@ -6,6 +6,7 @@ require_once 'paladin.php';
 require_once 'monstre.php';
 require_once 'jeu.php';
 require_once 'salledonjon.php';
+
 $personnage = isset($_POST['personnage'])?$_POST['personnage']:'';
 $choix = $_POST["fight"];
 $jeu = unserialize($_SESSION['jeu']);
@@ -15,13 +16,13 @@ $monstre = unserialize($_SESSION['monstre']);
 if($choix == 'fight') {
     $lastperso->attaque($monstre);
     if ($monstre->vie > 0) {
-        echo "le monstre a " . $monstre->vie . " de vie";
+
     } else {
         echo "Le monstre est mort";
     }
     $monstre->attaque($lastperso);
     if ($lastperso->vie > 0) {
-        echo "<br> le perso a " . $lastperso->vie . "de vie";
+       
 
     } else {
         echo "<br> Le " . $lastperso->nom . " est mort";
